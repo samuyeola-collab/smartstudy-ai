ee4da1c2e54645ff8c7e0029ba28d325c78a5aa1
Samiksha Yeola <samuyeola@gmail.com>
2026-05-08 14:07:14 +0530

    Fixed Render port

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, redirect, session
 
 from flask_session import Session
@@ -481,5 +483,8 @@ def download_notes(topic):
 
 
 # Run Flask App
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
